614142f32b4fb5af0892c7bc498a4eeea07e1e9a
Ji <tmddus921@gmail.com>
2023-04-24 22:34:46 +0900

    add aop package

--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.example.account.controller;
 
+import com.example.account.aop.AccountLock;
 import com.example.account.dto.CancelBalance;
 import com.example.account.dto.QueryTransactionResponse;
 import com.example.account.dto.TransactionDto;
@@ -25,6 +26,7 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping("/transaction/use")
+    @AccountLock
     public UseBalance.Response useBalance(
             @Valid @RequestBody UseBalance.Request request
     ) {
@@ -46,6 +48,7 @@ public class TransactionController {
     }
 
     @PostMapping("/transaction/cancel")
+    @AccountLock
     public CancelBalance.Response cancelBalance(
             @Valid @RequestBody CancelBalance.Request request
     ) {
